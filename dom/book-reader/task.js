f140fbabcef5027040eb934eacb768086fcfb82b
Lana <yarullina.1997@list.ru>
2023-03-14 02:26:46 +0300

    bhj tasks fix

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -9,19 +9,20 @@ sizeArray.forEach((e, i) => {
 });
 
 function showSize(i) {
-  sizeArray.forEach((e, i) => {
-    if (i == i) {
+  sizeArray.forEach((e, index) => {
+    if (i == index) {
       e.classList.add('font-size_active'); 
-      } else {
-        e.classList.remove('font-size_active');
-        }
-    })
-    if (i == 0) {
-      reader.classList.add('book_fs-small')
-      } else if (i == 2) {
-        reader.classList.remove('book_fs-small')
-        reader.classList.add('book_fs-big')
     } else {
-      reader.classList.remove('book_fs-small','book_fs-big' )
+      e.classList.remove('font-size_active');
     }
+  })
+
+  if (i == 0) {
+    reader.classList.add('book_fs-small');
+  } else if (i == 2) {
+    reader.classList.remove('book_fs-small')
+    reader.classList.add('book_fs-big')
+  } else {
+    reader.classList.remove('book_fs-small','book_fs-big' )
+  }
 }
