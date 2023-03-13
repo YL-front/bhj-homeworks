f0c226d005a0987ec0c3cb74bd7f0017a1d4c03e
Lana <yarullina.1997@list.ru>
2023-03-14 01:40:41 +0300

    bhj tasks fix

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,17 +1,37 @@
-let caseArray = Array.from(document.querySelectorAll('.rotator__case'));
+//let caseArray = Array.from(document.querySelectorAll('.rotator__case'));
 
-let rotate = function() {
+// let rotate = function() {
+//     let activeCaseIndex = caseArray.findIndex((e) => {
+//         return e.classList.contains('rotator__case_active')
+//     });
+
+//     caseArray[activeCaseIndex].classList.remove('rotator__case_active');
+
+//     if (activeCaseIndex < (caseArray.length - 1)) {
+//         caseArray[activeCaseIndex + 1].classList.add('rotator__case_active');
+//     } else {
+//         caseArray[0].classList.add('rotator__case_active'); 
+//     } 
+// }
+
+let caseArray = Array.from(document.querySelectorAll('.rotator__case'));
+let cases = document.querySelector('.rotator');
+
+
+let rotate = function() {                
+    
     let activeCaseIndex = caseArray.findIndex((e) => {
         return e.classList.contains('rotator__case_active')
     });
 
-    caseArray[activeCaseIndex].classList.remove('rotator__case_active');
-
-    if (activeCaseIndex < (caseArray.length - 1)) {
-        caseArray[activeCaseIndex + 1].classList.add('rotator__case_active');
+    if (caseArray[activeCaseIndex].nextElementSibling == null) {
+        caseArray[activeCaseIndex].classList.remove('rotator__case_active');
+        cases.firstElementChild.classList.add('rotator__case_active');
     } else {
-        caseArray[0].classList.add('rotator__case_active'); 
+        caseArray[activeCaseIndex].classList.remove('rotator__case_active');
+        caseArray[activeCaseIndex].nextElementSibling.classList.add('rotator__case_active');   
     } 
+
 }
 
 let timer = setInterval(rotate, 1000);
--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,26 @@
 let div = document.querySelectorAll('.reveal');
 
+// document.addEventListener('scroll', () => {
+//   div.forEach((e) => {
+//     const height = window.innerHeight;
+//     const top = e.getBoundingClientRect().top;
+//     if (top < height) {
+//       e.classList.add('reveal_active');
+//       } else {
+//         e.classList.remove('reveal_active');
+//       }
+//    })
+// });
+
 document.addEventListener('scroll', () => {
-  div.forEach((e) => {
-    const height = window.innerHeight;
-    const top = e.getBoundingClientRect().top;
-    if (top < height) {
-      e.classList.add('reveal_active');
-      } else {
-        e.classList.remove('reveal_active');
-      }
-   })
+  div.forEach((reveal) => {
+    const { innerHeight } = window;
+    const { top } = reveal.getBoundingClientRect();
+
+    if (top < innerHeight && top > 0) {
+      reveal.classList.add("reveal_active");
+    } else {
+      reveal.classList.remove("reveal_active");
+    }
+  })
 });
